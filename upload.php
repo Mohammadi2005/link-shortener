7c4979a4418c4cfdefebf79b553baef98c447219
amir hossein <amohammadi161183@gmail.com>
2025-09-14 16:04:23 +0330

    show image

--- upload.php
+++ upload.php
@@ -1,64 +1,70 @@
 <?php
+    $pdo = require_once "config/database.php";
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+    // save image
+    if(isset($_POST["submit"])) {
     $target_dir = "uploads/";
     $name_saved = time() . "-" . basename($_FILES["fileToUpload"]["name"]);
     $target_file = $target_dir . $name_saved;
     $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
+    if ($check !== false) {
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
         $uploadOk = 0;
     }
-}
 
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
 
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
 
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "<p class='alert alert-danger'>" . "Sorry, your file was not uploaded." . "</p>";
+    if ($uploadOk == 0) {
+        echo "<p class='alert alert-danger'>" . "Sorry, your file was not uploaded." . "</p>";
 // if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "<p class='alert alert-success'>" . "فایل شما با نام : ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). "با موفقیت اپلود شد." . "</p>";
-
-        $pdo = require_once "config/database.php";
-
-        try {
-            $sql = "insert into files (name, create_time) values (?, ?)";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$name_saved, time()]);
-
-        } catch (Exception $e) {
-            echo '<p class="alert alert-danger">Caught exception: ' . $e->getMessage() . '</p>';
-        }
-
     } else {
-        echo "<p class='alert alert-danger'>" ."Sorry, there was an error uploading your file." . "</p>";
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "<p class='alert alert-success'>" . "فایل شما با نام : " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . "با موفقیت اپلود شد." . "</p>";
+
+            try {
+                $sql = "insert into files (name, create_time) values (?, ?)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute([$name_saved, time()]);
+
+            } catch (Exception $e) {
+                echo '<p class="alert alert-danger">Caught exception: ' . $e->getMessage() . '</p>';
+            }
+
+        } else {
+            echo "<p class='alert alert-danger'>" . "Sorry, there was an error uploading your file." . "</p>";
+        }
     }
 }
+
+    // get all
+    $query = "select * from files";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
@@ -118,15 +124,37 @@ if ($uploadOk == 0) {
 
 <html>
 <body>
-
-<form action="upload.php" method="post" class="mt-5" enctype="multipart/form-data">
-    انتخاب تصویر :
-    <span class="btn btn-primary btn-file m-3">
+    <div class="container">
+        <form action="upload.php" method="post" class="mt-5" enctype="multipart/form-data">
+            انتخاب تصویر :
+            <span class="btn btn-primary btn-file m-3">
     انتخاب ...<input type="file" name="fileToUpload" id="fileToUpload">
     </span>
-  <input type="submit" value="اپلود تصویر" class="btn btn-success" name="submit">
-</form>
+            <input type="submit" value="اپلود تصویر" class="btn btn-success" name="submit">
+        </form>
 
+        <table class="table mt-3">
+            <thead class="table-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">نام</th>
+                <th scope="col">عملیات</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($result as $item) { ?>
+                <tr>
+                    <th scope="row"> <?php echo $item["id"] ?> </th>
+                    <td> <?php echo $item['name'] ?></td>
+                    <td>
+                        <a class="btn btn-primary" href="./uploads/<?php echo $item['name'] ?>" >نمایش</a>
+                        <a class="btn btn-danger" href="./" >حذف</a>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </html>
