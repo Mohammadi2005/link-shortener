fac25c9bd0317e8dee599a6f152cb6411671d78c
amir hossein <amohammadi161183@gmail.com>
2025-09-14 15:19:07 +0330

    upload file backend

--- upload.php
+++ upload.php
@@ -1,3 +1,53 @@
+<?php
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . time() . "-" . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "<p class='alert alert-danger'>" . "Sorry, your file was not uploaded." . "</p>";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "<p class='alert alert-success'>" . "فایل شما با نام : ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). "با موفقیت اپلود شد." . "</p>";
+    } else {
+        echo "<p class='alert alert-danger'>" ."Sorry, there was an error uploading your file." . "</p>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -59,7 +109,7 @@
 <form action="upload.php" method="post" class="mt-5" enctype="multipart/form-data">
     انتخاب تصویر :
     <span class="btn btn-primary btn-file m-3">
-    انتخاب ...<input type="file">
+    انتخاب ...<input type="file" name="fileToUpload" id="fileToUpload">
     </span>
   <input type="submit" value="اپلود تصویر" class="btn btn-success" name="submit">
 </form>
