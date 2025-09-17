b1f978efcdac1ff3ad1f6b27699609a99dcce167
amir hossein <amohammadi161183@gmail.com>
2025-09-17 11:21:57 +0330

    validation

--- contact.php
+++ contact.php
@@ -1,23 +1,93 @@
 <?php
-    if(isset($_POST['send'])){
+
+    $old_name = "";
+    $old_email = "";
+    $old_phone = "";
+    $old_subject = "";
+    $old_message = "";
+
+    if(isset($_POST['submit'])){
         $name = $_POST['name'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
         $subject = $_POST['subject'];
         $message = $_POST['message'];
 
-        $pdo = require_once "config/database.php";
+        // validation
+        $validationMsg = [
+            "message" => [],
+            "class" => null,
+        ];
+        $validationError = false;
 
-        try {
-            $sql = "insert into contact (name, email, phone, subject, message) values (?, ?, ?, ?, ?)";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$name, $email, $phone, $subject, $message]);
+        // validation name
+        if (empty($name)) {
+            $msg = "نام خالی هست";
+            array_push($validationMsg['message'],$msg);
+        } elseif (strlen($name) < 3) {
+            $msg = "طول نام حداقل باید 3 باشد";
+            array_push($validationMsg['message'],$msg);
+        }
 
-            if ($stmt) {
-                echo '<p class="alert alert-success text-center">پیام با موفقیت ارسال شد</p>';
+        // validation email
+        function validate_email($email)
+        {
+            return filter_var($email, FILTER_VALIDATE_EMAIL);
+        }
+
+        if (empty($email)) {
+            $msg = "ایمیل خالی هست";
+            array_push($validationMsg['message'],$msg);
+        } elseif (!validate_email($email)) {
+            $msg = "فرمت ایمیل اشتباه است.";
+            array_push($validationMsg['message'],$msg);
+        }
+
+
+        // validation phone
+        function validate_mobile($mobile)
+        {
+            return preg_match('/^0[0-9]{10}+$/', $mobile);
+        }
+
+        if (empty($phone)) {
+            $msg = "تلفن تماس خالی هست.";
+            array_push($validationMsg['message'],$msg);
+        } elseif (!validate_mobile($phone)) {
+            $msg = "تلفن تماس باید 11 رقم باشد و با صفر شروع شود.";
+            array_push($validationMsg['message'],$msg);
+        }
+
+
+        if (count($validationMsg['message']) > 0) {
+            $validationMsg['class'] .= 'alert alert-danger';
+            $validationError = true;
+        }
+
+        if (!$validationError) {
+            $pdo = require_once "config/database.php";
+
+            try {
+                $sql = "insert into contact (name, email, phone, subject, message) values (?, ?, ?, ?, ?)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute([$name, $email, $phone, $subject, $message]);
+
+                if ($stmt) {
+                    $msg = 'پیام با موفقیت ارسال شد';
+                    array_push($validationMsg['message'],$msg);
+                    $validationMsg['class'] = 'alert alert-success';
+                }
+            } catch (Exception $e) {
+                $msg = 'Caught exception: ' . $e->getMessage();
+                array_push($validationMsg['message'],$msg);
+                $validationMsg['class'] = 'alert alert-danger';
             }
-        } catch (Exception $e) {
-            echo '<p class="alert alert-danger">Caught exception: ' . $e->getMessage() . '</p>';
+        } else {
+            $old_name = $name;
+            $old_email = $email;
+            $old_phone = $phone;
+            $old_subject = $subject;
+            $old_message = $message;
         }
     }
 ?>
@@ -67,18 +137,28 @@
     </style>
 </head>
 <body>
-<h2>تماس با ما</h2>
+    <div class="container w-50">
+        <h2>تماس با ما</h2>
 
-    <form id="shorten" method="post">
-        <input type="text" class="form-control" name="name" id="endpointLink" placeholder="نام" required>
-        <input type="email" class="form-control" name="email" id="endpointLink" placeholder="ایمیل" required>
-        <input type="phone" class="form-control" name="phone" id="endpointLink" placeholder="تلفن تماس">
-        <input type="subject" class="form-control" name="subject" id="endpointLink" placeholder="موضوع پیام" required>
-        <textarea name="message" class="form-control" id="endpointLink" required rows="4"></textarea>
-        <br>
-        <button type="submit" name="send" id="shorten-button">ارسال</button>
-    </form>
-<div id="shorten-result"></div>
+        <form id="shorten" method="post">
+            <input type="text" class="form-control" name="name" id="endpointLink" value="<?php echo $old_name ?>" placeholder="نام">
+            <input type="text" class="form-control" name="email" id="endpointLink" value="<?php echo $old_email ?>" placeholder="ایمیل">
+            <input type="phone" class="form-control" name="phone" id="endpointLink" value="<?php echo $old_phone ?>" placeholder="تلفن تماس">
+            <input type="subject" class="form-control" name="subject" id="endpointLink" value="<?php echo $old_subject ?>" placeholder="موضوع پیام">
+            <textarea name="message" class="form-control" id="endpointLink" rows="4"><?php echo $old_message ?></textarea>
+            <br>
+            <button type="submit" name="submit" id="shorten-button">ارسال</button>
+        </form>
+        <div>
+            <br>
+
+            <?php if (isset($_POST['submit'])) { ?>
+                <?php foreach ($validationMsg['message'] as $message) { ?>
+                    <p class="<?php echo $validationMsg['class'] ?>" ><?php echo $message ?></p>
+                <?php } ?>
+            <?php } ?>
+        </div>
+    </div>
 
 <footer>
     <a href="index.php">بازگشت</a>
